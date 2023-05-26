bc44092abf21d7913cdf24528f633e86799ce17e
darryllgoh <darryllgoh@gmail.com>
2023-05-26 16:43:37 +0800

    final updates + deploy

---
+++ target/classes/static/js/formController.js
@@ -0,0 +1,37 @@
+//Create object from form & call fetch method to POST to backend
+function addProduct(name, description, brand, category, usSizes, color, price, SKU, imgMain, imgHover, imageObjectMain, imageObjectHover)
+{
+   const formData = new FormData();
+   formData.append('name', name);
+   formData.append('description', description);
+   formData.append('brand', brand);
+   formData.append('category', category);
+   formData.append('usSize', usSizes);
+   formData.append('color', color);
+   formData.append('price', price);
+   formData.append('SKU', SKU);
+   formData.append('imgMain', imgMain);
+   formData.append('imgHover', imgHover);
+   formData.append('imageMainfile',imageObjectMain);
+   formData.append('imageHoverfile',imageObjectHover);
+
+  fetch(addAPI, {
+        method: 'POST',
+        body: formData
+        })
+        .then(function(response) {
+            console.log(response.status); // Will show you the status
+            if (response.ok) {
+                alert("Successfully Added Product!")
+                document.getElementById("productForm").reset();
+                window.location.href = "/upload";
+            }
+            else {
+               alert("Please complete all fields before submission")
+            }
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            alert("Error adding item to Product")
+        });
+}
